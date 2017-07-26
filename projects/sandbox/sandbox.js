7666bfff57b501032bdd63f4263079fe95009646
Wang Min <Wangsik@gmail.com>
2017-07-25 20:53:57 -0400

    JQ

---
+++ projects/sandbox/sandbox.js
@@ -0,0 +1,32 @@
+
+var options = {
+    "font-size": "300%",
+    "margin-left": "auto",
+    "padding": "auto"
+};
+var options2 = {
+    "font-size": "100%",
+    "margin-left": "auto",
+    "padding": "auto"
+};
+var options3 = {
+    "margin-left": "10em",
+    "padding": "auto"
+};
+var options4 = {
+    "margin-left": "0em",
+    "padding": "auto"
+};
+var options5 = {
+    "margin-left": "1em",
+    "padding": "auto"
+};
+var options6 = {
+    "margin-left": "0.5em",
+    "padding": "auto"
+};
+
+
+$("#change-me").animate( options ).animate( options2 );
+$("#secret").animate( options2 ).animate( options ).animate( options2 );
+$("h1").animate( options3 ).animate( options4 ).animate( options5 ).animate( options4 ).animate( options6 ).animate( options4 );
